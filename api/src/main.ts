f75f1c84b0bb0a69706baa2de53e15cab5081a41
Daniel Felix <carlosdanielfelixfarias@gmail.com>
2025-12-24 18:19:49 -0300

    feat(api): adição de validações globais
    - Importado o ValidationPipe do NestJS.
    - Configurado o ValidationPipe para:
      - Remover propriedades não decoradas.
      - Lançar erro se houver propriedades extras.
      - Transformar payloads para DTOs.
      - Habilitar conversão implícita de tipos.
    
    Essas alterações melhoram a validação de dados na aplicação, garantindo que apenas dados válidos sejam processados, o que aumenta a segurança e a robustez do sistema.

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Remove proprieades não decoradas
+      forbidNonWhitelisted: true, // Lança erro se houver propriedades extras
+      transform: true, // Transforma payloads para DTOs
+      transformOptions: {
+        enableImplicitConversion: true, // Habilita conversão implícita de tipos
+      },
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
