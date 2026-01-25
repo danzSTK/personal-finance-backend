47860dd1b7b2c87a517c9504ef30a18a321b760f
Daniel Felix <carlosdanielfelixfarias@gmail.com>
2026-01-25 02:06:49 -0300

    feat(decorator): adicionar decorador CurrentUser
    - Criado o decorador CurrentUser para facilitar a obtenção do usuário atual a partir do contexto da requisição.
    - Implementado tratamento de erro caso o usuário não seja encontrado na requisição.
    
    Essa adição melhora a legibilidade e a reutilização do código, permitindo que o usuário atual seja acessado de forma consistente em diferentes partes da aplicação.

---
+++ api/src/common/decorators/current-user.decorator.ts
@@ -0,0 +1,17 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
+import { Request } from 'express';
+import { User } from '../../modules/users/entities/user.entity';
+
+export const CurrentUser = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext): User => {
+    const request = ctx.switchToHttp().getRequest<Request>();
+    const user = request.user as User;
+
+    if (!user) {
+      throw new Error('User not found in request');
+    }
+
+    return user;
+  },
+);
