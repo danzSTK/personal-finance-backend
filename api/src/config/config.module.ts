657662be88e73e38559c6414a7c47c51359d7024
Daniel Felix <carlosdanielfelixfarias@gmail.com>
2026-01-25 02:09:06 -0300

    feat(config): adicionar configurações de OAuth e Redis
    - Criado o arquivo google-oauth.config.ts para gerenciar as configurações do Google OAuth.
    - Atualizado o config.module.ts para incluir googleOauthConfig e redisConfig.
    - Adicionadas novas variáveis de ambiente para Google OAuth e Redis no config.module.ts.
    - Atualizado o jwt.config.ts para incluir a variável APP_URL como emissor.
    
    Essas alterações melhoram a configuração do aplicativo, permitindo a integração com o Google OAuth e a utilização do Redis para cache, aumentando a flexibilidade e a escalabilidade do sistema.

--- api/src/config/config.module.ts
+++ api/src/config/config.module.ts
@@ -4,12 +4,14 @@ import { join } from 'path';
 import Joi from 'joi';
 import databaseConfig from './database.config';
 import jwtConfig from './jwt.config';
+import googleOauthConfig from './google-oauth.config';
+import redisConfig from './redis/redis.config';
 
 @Module({
   imports: [
     NestConfigModule.forRoot({
       cache: true,
-      load: [databaseConfig, jwtConfig],
+      load: [databaseConfig, jwtConfig, googleOauthConfig, redisConfig],
       envFilePath: join(process.cwd(), '..', '.env'),
       isGlobal: true,
       validationSchema: Joi.object({
@@ -27,6 +29,23 @@ import jwtConfig from './jwt.config';
         JWT_REFRESH_SECRET: Joi.string().min(32).required(),
         JWT_ACCESS_EXPIRES_IN: Joi.string().required(),
         JWT_REFRESH_EXPIRES_IN: Joi.string().required(),
+        APP_URL: Joi.string().uri().default('http://localhost:3000'),
+
+        // google oauth
+        GOOGLE_CLIENT_ID: Joi.string().required(),
+        GOOGLE_CLIENT_SECRET: Joi.string().required(),
+        GOOGLE_CALLBACK_URL: Joi.string()
+          .uri()
+          .default('http://localhost:3000/auth/google/callback'),
+
+        // frontend url
+        FRONTEND_URL: Joi.string().uri().default('http://localhost:3001'),
+
+        // redis
+        REDIS_HOST: Joi.string().default('localhost'),
+        REDIS_PORT: Joi.number().default(6379),
+        REDIS_PASSWORD: Joi.string().required(),
+        REDIS_TTL: Joi.number().default(3600),
       }),
       validationOptions: {
         abortEarly: true,
