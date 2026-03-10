729f015a85136fc2ced8d6f37e6521e9071ca896
Daniel Felix <carlosdanielfelixfarias@gmail.com>
2026-03-10 12:47:50 -0300

    refactor(decorators): corrigir importações e adicionar constantes de usuário
    - Atualizado o caminho de importação do User no decorator CurrentUser para refletir a nova estrutura de diretórios.
    - Adicionada a exportação de constantes de usuário no arquivo index.ts.
    - Criado o arquivo user.constants.ts com expressões regulares e limites de comprimento para nome e e-mail de usuário.
    - Corrigido o caminho de importação do User na interface AuthRequest.
    
    Essas alterações melhoram a organização do código, garantindo que as importações estejam corretas e centralizando as constantes relacionadas ao usuário, o que facilita a manutenção e a legibilidade do projeto.

---
+++ api/src/common/models/constants/user.constants.ts
@@ -0,0 +1,7 @@
+export const USER_NAME_REGEX = /^[a-zA-Z0-9_-]+$/;
+export const USER_EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+export const USER_NAME_MIN_LENGTH = 3;
+export const USER_NAME_MAX_LENGTH = 50;
+
+export const USER_EMAIL_MAX_LENGTH = 255;
