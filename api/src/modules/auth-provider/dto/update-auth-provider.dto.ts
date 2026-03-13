c3d73cec1243da47fc4c99bcfa2eb27220e09536
Daniel Felix <carlosdanielfelixfarias@gmail.com>
2026-03-13 17:41:55 -0300

    refactor(auth-provider): remover módulo AuthProvider
    - Removido o módulo AuthProviderModule e suas dependências do projeto.
    - Deletados os arquivos auth-provider.controller.ts, auth-provider.service.ts e auth-provider.module.ts.
    - Removidos os DTOs CreateAuthProviderDto, UpdateAuthProviderDto e GoogleUserProfileDto.
    
    Essa mudança consolida a lógica de provedores de autenticação no módulo de usuários, eliminando redundâncias e melhorando a organização do código conforme a nova estrutura de dom

--- api/src/modules/auth-provider/dto/update-auth-provider.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateAuthProviderDto } from './create-auth-provider.dto';
-
-export class UpdateAuthProviderDto extends PartialType(CreateAuthProviderDto) {}
