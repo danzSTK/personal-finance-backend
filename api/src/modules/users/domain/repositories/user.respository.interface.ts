c005ac38abc831f52a98e5211a7eeea446370ba0
Daniel Felix <carlosdanielfelixfarias@gmail.com>
2026-03-13 17:44:00 -0300

    refactor(users): adicionar método findByAuthProvider ao repositório de usuários
    - Adicionada importação de AuthProviderType do módulo common/models/enums.
    - Implementado método abstrato findByAuthProvider na interface IUserRepository.
    - Método aceita provider (AuthProviderType), providerUserId (string) e options opcionais.
    - Retorna Promise<User | null> para buscar usuários por provedor de autenticação.
    
    Essa mudança permite a busca de usuários através de provedores de autenticação externos

--- api/src/modules/users/domain/repositories/user.respository.interface.ts
+++ api/src/modules/users/domain/repositories/user.respository.interface.ts
@@ -2,6 +2,7 @@ import { EntityManager } from 'typeorm';
 import { User } from '../entities/user.entity';
 import { UserName } from '../value-objects/user-name.value-object';
 import { Email } from '../value-objects/email.value-object';
+import { AuthProviderType } from '../../../../common/models/enums';
 
 export interface IRepositoryOptions {
   manager?: EntityManager;
@@ -11,5 +12,10 @@ export abstract class IUserRepository {
   abstract findById(id: string, options?: IRepositoryOptions): Promise<User | null>;
   abstract findByEmail(email: Email, options?: IRepositoryOptions): Promise<User | null>;
   abstract findByUserName(userName: UserName, options?: IRepositoryOptions): Promise<User | null>;
+  abstract findByAuthProvider(
+    provider: AuthProviderType,
+    providerUserId: string,
+    options?: IRepositoryOptions,
+  ): Promise<User | null>;
   abstract save(user: User, options?: IRepositoryOptions): Promise<User>;
 }
